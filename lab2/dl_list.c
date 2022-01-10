3015a2cdc476e82f336b3233d8b48868bbe5340f
noChowder <bryankchau@gmail.com>
2022-01-10 12:02:11 -0800

    created struct & main

--- lab2/dl_list.c
+++ lab2/dl_list.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-struct listelement{
-    listelement *next, *prev;
+struct string{
+    char *next, *prev;
     char text[1000];
 };
-listelement *head = NULL;
+char *head = NULL;
+
+int main(){
+    printf("Select: \n1 push string \n2 print list \n3 delete item \n4 end program \n");
+    int selection;
+    scanf("%d", &selection);
+}
