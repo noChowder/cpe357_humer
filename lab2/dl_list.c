721e4fe2464630bb3201aa1695d9bd19babb267b
noChowder <bryankchau@gmail.com>
2022-01-13 23:43:37 -0800

    working push

--- lab2/dl_list.c
+++ lab2/dl_list.c
@@ -10,27 +10,46 @@ struct node{
 };
 struct node *head = NULL;
 
-int push_string(struct node *node, char *string){
+int push_string(char *string){
+    struct node *node = malloc(sizeof(struct node));
     if(head == NULL){
-        node = malloc(SIZE*sizeof(char));
+        printf("here1 \n");
         strcpy(node->text, string);
         node->next = NULL;
+        head = node;
     }
-    else if(head != NULL){
-        if(node == NULL){
-            strcpy(node->text, string);
-            node->next = NULL;
+    else{
+        //struct node *next = malloc(sizeof(struct node));
+        //node = head->next;
+        //node->prev = head;
+        struct node *ptr = head;
+        printf("here2 \n");
+        while(ptr != NULL){
+            printf("here3 \n");
+            ptr = ptr->next;
         }
-        else{
-            node->next->prev = node;
-            push_string(node->next, string);
+        if(ptr == NULL){
+            printf("here4 \n");
+            strcpy(node->text, string);
+            printf("here5 \n");
+            node->next = NULL;
+            printf("here6 \n");
+            ptr = node;
         }
     }
 
+    free(node);
     return 0;
 }
 
 int print_list(){
+    printf("here\n");
+    struct node *ptr = head;
+    while(ptr != NULL){
+        printf("here1\n");
+        printf("%s \n", ptr->text);
+        ptr = ptr->next;
+    }
     return 0;
 }
 
@@ -42,12 +61,14 @@ int main(){
         printf("insert text \n");
         char string[SIZE];
         scanf("%s", &string);
-        push_string(head, string);
+        push_string(string);
         main();
     }
     else if(sel == 2){
         print_list();
         main();
     }
+
+    //free(node);
     return 0;
 }
