eb9aa7212ec585144cde92d8e2ae333564346f7d
Chowder <bryankchau@gmail.com>
2022-01-18 14:27:11 -0800

    open files

--- project1/blend.c
+++ project1/blend.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef unsigned char byte;
+
+int main(int argc, char *argv[]){
+    FILE *im1, *im2, *test;
+    byte buff[256];
+
+    im1 = fopen("lion.bmp", "r");
+    test = fopen("test.txt", "w");
+    fgets(buff, 2, im1);
+    printf("%s\n", buff);
+
+    fclose(im1);
+    fclose(test);
+    return 0;
+}
