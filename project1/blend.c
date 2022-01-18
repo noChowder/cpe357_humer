4c7cc01bf70a78e922fc35cd11f94899121c2608
Chowder <bryankchau@gmail.com>
2022-01-18 15:55:04 -0800

    read file header

--- project1/blend.c
+++ project1/blend.c
@@ -3,15 +3,44 @@
 #include <string.h>
 
 typedef unsigned char byte;
+typedef unsigned short WORD;
+typedef unsigned int DWORD;
+typedef unsigned int LONG;
+
+struct tagBITMAPFILEHEADER{
+    WORD bfType;  //specifies the file type 
+    DWORD bfSize;  //specifies the size in bytes of the bitmap file 
+    WORD bfReserved1;  //reserved; must be 0 
+    WORD bfReserved2;  //reserved; must be 0 
+    DWORD bfOffBits;  //species the offset in bytes from the bitmapfileheader to the bitmap bits 
+}typedef BITMAPFILEHEADER;
+
+struct tagBITMAPINFOHEADER{ 
+    DWORD biSize;  //specifies the number of bytes required by the struct 
+    LONG biWidth;  //specifies width in pixels 
+    LONG biHeight;  //species height in pixels 
+    WORD biPlanes; //specifies the number of color planes, must be 1 
+    WORD biBitCount; //specifies the number of bit per pixel 
+    DWORD biCompression;//spcifies the type of compression 
+    DWORD biSizeImage;  //size of image in bytes 
+    LONG biXPelsPerMeter;  //number of pixels per meter in x axis 
+    LONG biYPelsPerMeter;  //number of pixels per meter in y axis 
+    DWORD biClrUsed;  //number of colors used by th ebitmap 
+    DWORD biClrImportant;  //number of colors that are important 
+}typedef BITMAPINFOHEADER; 
 
 int main(int argc, char *argv[]){
     FILE *im1, *im2, *test;
-    byte buff[256];
+    BITMAPFILEHEADER bmpFileHeader;
 
-    im1 = fopen("lion.bmp", "r");
+    im1 = fopen("lion.bmp", "rb");
     test = fopen("test.txt", "w");
-    fgets(buff, 2, im1);
-    printf("%s\n", buff);
+    fread(&bmpFileHeader, sizeof(BITMAPFILEHEADER), 1, im1);
+    if(bmpFileHeader.bfType != 0x4D42){
+        printf("here\n");
+    }
+    printf("%X \n", bmpFileHeader.bfType);
+    printf("%X \n", bmpFileHeader.bfSize);
 
     fclose(im1);
     fclose(test);
