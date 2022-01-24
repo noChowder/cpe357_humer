ea9a629fe3a5a2d6d48736f51b591a177cd9c159
Chowder <bryankchau@gmail.com>
2022-01-23 17:43:31 -0800

    create functions

--- lab3/dma.c
+++ lab3/dma.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    int *i;
-    *i = 0;
-    return 0;
+unsigned char myheap[1048576];
+#define PAGESIZE 1024
+
+typedef struct chunkhead{
+    unsigned int size;
+    unsigned int info;
+    unsigned char *next, *prev;
+}chunkhead;
+
+unsigned char *mymalloc(unsigned int size){
+    int n = 1;
+
+    return ;
+}
+
+void myfree(unsigned char *address){
+}
+
+void analyse(){
+
+}
+
+void main(){
+    unsigned char *a, *b, *c;
+    a = mymalloc(1000);
+    b = mymalloc(1000);
+    c = mymalloc(1000);
+    myfree(b);
+    myfree(a);
+    analyse();
 }
