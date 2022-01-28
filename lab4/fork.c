0505b86bd69871629d5bed729044875cd45c31b2
Chowder <bryankchau@gmail.com>
2022-01-27 16:49:08 -0800

    added lab4

---
+++ lab4/fork.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+typedef unsigned short WORD;
+typedef unsigned int DWORD;
+typedef unsigned int LONG;
+typedef unsigned char BYTE;
+
+typedef struct tagBITMAPFILEHEADER{
+    WORD bfType;  //specifies the file type 
+    DWORD bfSize;  //specifies the size in bytes of the bitmap file 
+    WORD bfReserved1;  //reserved; must be 0 
+    WORD bfReserved2;  //reserved; must be 0 
+    DWORD bfOffBits;  //species the offset in bytes from the bitmapfileheader to the bitmap bits 
+}BITMAPFILEHEADER;
+
+typedef struct tagBITMAPINFOHEADER{ 
+    DWORD biSize;  //specifies the number of bytes required by the struct 
+    LONG biWidth;  //specifies width in pixels 
+    LONG biHeight;  //species height in pixels 
+    WORD biPlanes; //specifies the number of color planes, must be 1 
+    WORD biBitCount; //specifies the number of bit per pixel 
+    DWORD biCompression;//spcifies the type of compression 
+    DWORD biSizeImage;  //size of image in bytes 
+    LONG biXPelsPerMeter;  //number of pixels per meter in x axis 
+    LONG biYPelsPerMeter;  //number of pixels per meter in y axis 
+    DWORD biClrUsed;  //number of colors used by the bitmap 
+    DWORD biClrImportant;  //number of colors that are important 
+}BITMAPINFOHEADER;
