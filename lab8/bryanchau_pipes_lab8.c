cf5dc0d73aeb579d90bce88ff3249e64cdddf1cd
Chowder <bryankchau@gmail.com>
2022-02-25 11:05:56 -0800

    old

--- lab8/bryanchau_pipes_lab8.c
+++ lab8/bryanchau_pipes_lab8.c
@@ -22,3 +22,20 @@ void init_pipe(mypipe *pipe, int size);
 int mywrite(mypipe *pipe, byte *buffer, int size);
 
 int myread(mypipe *pipe, byte *buffer, int size);
+
+void main(){
+    char text[100];
+    mypipe pipeA;
+    init(&pipeA, 32);
+    mywrite(&pipeA, "hello world", 12);
+    mywrite(&pipeA, "it's a nice day", 16);
+
+    myread(&pipeA, text, 12);
+    printf("%s \n", text);
+    myread(&pipeA, text, 16);
+    printf("%s \n", text);
+
+    mywrite(&pipeA, "and now we test the carryover", 30);
+    myread(&pipeA, text, 30);
+    printf("%s \n", text);
+}
