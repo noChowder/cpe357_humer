b149e7367fc0a83df3e91481abd3e52db79d9dce
Chowder <bryankchau@gmail.com>
2022-03-02 18:30:04 -0800

    create project 5

---
+++ project5/mpi.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#define MATRIX_DIMENSION_XY 10
