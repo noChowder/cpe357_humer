53156d5e578152ff2f7e8776a5a343fce97d82e7
Chowder <bryankchau@gmail.com>
2022-03-15 17:01:31 -0700

    weird newline

--- final/countdown.c
+++ final/countdown.c
@@ -11,18 +11,61 @@
 #include <time.h>
 #include <sys/time.h>
 
-int main(int argc, char *argv[]){
-    float time2launch = atof(argv[1]);
-    time2launch *= 2;
+#define INTERVAL 500
 
-    while(time2launch > 0){
-        for(int i = 0; i < time2launch; i++){
-            printf(".");
-        }
-        time2launch -= 1;
-        printf("\n");
-        usleep(500000);
+float time2launch = 0;
+
+void alarm_handler(int alarmsig){
+    for(int i = 0; i < time2launch; i++){
+        printf(".");
     }
+    printf("\n");
+    fflush(stdout);
+    // printf("alarm sent \n");
+}
 
+int main(int argc, char *argv[]){
+    signal(SIGALRM, alarm_handler);
+    time2launch = atof(argv[1]);
+    time2launch *= 2;
+    
+    struct itimerval alarm;
+    alarm.it_value.tv_sec = 0;
+    alarm.it_value.tv_usec = INTERVAL * 1000;
+    alarm.it_interval = alarm.it_value;
+    while(time2launch > 0){
+        if(setitimer(ITIMER_REAL, &alarm, NULL) != 0){
+            fprintf(stderr, "Error: setitimer() \n");
+            return -1;
+        }
+        pause();
+        time2launch -= 1;
+    }
+    //usleep(INTERVAL*1000);
+
+    if(argc > 2){
+    int cmdlines = argc - 1;
+    char *args[cmdlines];
+    for(int i = 0; i < cmdlines-1; i++){
+        args[i] = malloc(100);
+        strcpy(args[i], argv[i+2]);
+    }
+    args[cmdlines] = NULL;
+    // printf("here\n");
+    if(cmdlines > 0){
+        if(fork() == 0){
+            execv(args[0], args);
+        }
+    }
+    // else{
+    //     wait(0);
+    // }
+    wait(0);
+
+    for(int i = 0; i < cmdlines; i++){
+        // printf("%d", i);
+        free(args[i]);
+    }
+    }
     return 0;
 }
